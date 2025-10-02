b7912624e4443344734d1b1ff0684e55a7dca420
Vimal Sonagara <vimalsonagara978@gmail.com>
2025-10-02 19:23:36 +0530

    Serve React frontend from backend

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,15 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"))
 app.use('/api',require("./Routes/Displaydata"));
-app.use('/api',require("./Routes/OrderData"));
+app.use('/api',require("./Routes/OrderData"));  
 app.use('/api',require("./Routes/MyProfile"));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
